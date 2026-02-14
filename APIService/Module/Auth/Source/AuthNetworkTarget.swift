bf24778923f3dc2b2c30975f50f5cb98294e06ee
binaryloader <152517454+binaryloader@users.noreply.github.com>
2026-02-15 01:37:09 +0900

    feat: initial commit - Synstagram API service module
    인증 관련 API 서비스 모듈.
    hacoma → binaryloader 마이그레이션 완료.

---
+++ APIService/Module/Auth/Source/AuthNetworkTarget.swift
@@ -0,0 +1,74 @@
+//
+//  AuthNetworkTarget.swift
+//  Auth
+//
+//  Created by binaryloader on 2020/10/11.
+//
+
+import Foundation
+import BinaryloaderExtensions
+import BinaryloaderNetwork
+
+struct AuthNetworkTarget {
+    
+    enum Route {
+        case isAlreadyLogin
+        case login(request: AuthLoginModel.Request)
+        case logout
+    }
+    
+    private let route: Route
+    
+    init(route: Route) {
+        self.route = route
+    }
+}
+
+extension AuthNetworkTarget: NetworkTarget {
+    
+    var baseURL: URL {
+        let URLString = "https://photo.domain.com/photo/webapi"
+        return URLString.toAPIURL
+    }
+    
+    var path: String {
+        return "/auth.php"
+    }
+    
+    var method: BinaryloaderNetwork.Method {
+        return .get
+    }
+    
+    var sampleData: Data {
+        return Data()
+    }
+    
+    var task: Task {
+        var parameters: [String: Any] = [
+            "api": "SYNO.PhotoStation.Auth",
+            "version": "1"
+        ]
+        
+        switch route {
+        case .isAlreadyLogin:
+            parameters["method"] = "checkauth"
+        case .login(let request):
+            parameters["method"] = "login"
+            parameters["username"] = request.username
+            parameters["password"] = request.password
+            parameters["remember_me"] = request.isRemember ? "yes" : "no"
+        case .logout:
+            parameters["method"] = "logout"
+        }
+        
+        return .requestParameters(parameters: parameters, encoding: URLEncoding.queryString)
+    }
+    
+    var headers: [String: String]? {
+        return nil
+    }
+    
+    var timeoutInterval: TimeInterval {
+        return 10
+    }
+}
